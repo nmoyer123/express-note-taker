0884d241f4e3a6291467648c45db0901c43040b6
Noah Moyer <noahmoyer@icloud.com>
2024-05-16 18:25:39 -0700

    added server js with express and delete function

---
+++ Develop/server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+const notesFilePath = path.join(__dirname, '/db/db.json');
+
+// GET Route for retrieving all the notes
+app.get('/api/notes', (req, res) => {
+    fs.readFile(notesFilePath, 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).json(err);
+        } else {
+            res.json(JSON.parse(data));
+        }
+    });
+});
+
+// POST Route for a new note
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    fs.readFile(notesFilePath, 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).json(err);
+        } else {
+            const notes = JSON.parse(data);
+            newNote.id = notes.length > 0 ? Math.max(...notes.map(note => note.id)) + 1 : 1;
+            notes.push(newNote);
+            fs.writeFile(notesFilePath, JSON.stringify(notes, null, 2), (err) => {
+                if (err) {
+                    res.status(500).json(err);
+                } else {
+                    res.json(newNote);
+                }
+            });
+        }
+    });
+});
+
+// DELETE Route for a specific note
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = parseInt(req.params.id);
+  fs.readFile(notesFilePath, 'utf8', (err, data) => {
+      if (err) {
+          res.status(500).json(err);
+      } else {
+          const notes = JSON.parse(data);
+          const newNotes = notes.filter(note => note.id !== noteId);
+          fs.writeFile(notesFilePath, JSON.stringify(newNotes, null, 2), err => {
+              if (err) {
+                  res.status(500).json(err);
+              } else {
+                  res.json({ message: 'Deleted successfully' });
+              }
+          });
+      }
+  });
+});
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # express-note-taker
+
+## Description
+Note Taker is a simple, easy-to-use application designed for writing, saving, and managing notes. Built with an Express.js backend, this application can help small business owners and individuals alike keep track of tasks, ideas, and reminders by providing a digital notebook accessible via a web interface.
+
